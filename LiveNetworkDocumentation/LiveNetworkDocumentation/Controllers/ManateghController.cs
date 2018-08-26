cb3576e0fe06851fe8ca4ebe08aa6539446bc634
babak35102 <babak35102@gmail.com>
2018-08-26 16:35:04 +0430

    addModelEntityFramework

---
+++ LiveNetworkDocumentation/LiveNetworkDocumentation/Controllers/ManateghController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveNetworkDocumentation.Controllers
+{
+    public class ManateghController : Controller
+    {
+        // GET: Manategh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
