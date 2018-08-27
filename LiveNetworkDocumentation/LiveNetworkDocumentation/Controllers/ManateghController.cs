1300c334145109687e951269b93ba9e6a0eb9f6d
babak35102 <babak35102@gmail.com>
2018-08-27 16:23:19 +0430

    add manategh list / Edit / Save

--- LiveNetworkDocumentation/LiveNetworkDocumentation/Controllers/ManateghController.cs
+++ LiveNetworkDocumentation/LiveNetworkDocumentation/Controllers/ManateghController.cs
@@ -3,15 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LiveNetworkDocumentation.Models;
 
 namespace LiveNetworkDocumentation.Controllers
 {
     public class ManateghController : Controller
     {
+        private readonly LiveNetworkDocumentationEntities _db;
+
+        public ManateghController()
+        {
+            _db = new LiveNetworkDocumentationEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+
         // GET: Manategh
         public ActionResult Index()
         {
-            return View();
+            var manateghs = _db.Manateghs.ToList();
+
+            return View(manateghs);
+        }
+
+        public ActionResult Edit(byte id)
+        {
+            var mantaghe = _db.Manateghs.SingleOrDefault(m => m.Id == id);
+            if (mantaghe == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("MantagheForm", mantaghe);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Manategh manategh)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("MantagheForm", manategh);
+            }
+
+            if (manategh.Id == 0)        //  Create a new mantaghe
+            {
+                _db.Manateghs.Add(manategh);
+            }
+            else                            // update an existing mantaghe
+            {
+                var mantagheInDb = _db.Manateghs.Single(m => m.Id == manategh.Id);
+                mantagheInDb.Name = manategh.Name;
+                mantagheInDb.CityCenter = manategh.CityCenter;
+                mantagheInDb.Address = manategh.Address;
+                mantagheInDb.PreTelCode = manategh.PreTelCode;
+            }
+            
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index", "Manategh");
         }
     }
 }
