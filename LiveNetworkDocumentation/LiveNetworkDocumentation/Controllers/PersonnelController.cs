e74f714043052308e0116e25ad2e7ae72fdd92e7
babak35102 <babak35102@gmail.com>
2018-08-28 17:34:07 +0430

    Add Personnel Controller, show list of them

---
+++ LiveNetworkDocumentation/LiveNetworkDocumentation/Controllers/PersonnelController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using LiveNetworkDocumentation.Models;
+using LiveNetworkDocumentation.ViewModels;
+
+namespace LiveNetworkDocumentation.Controllers
+{
+    public class PersonnelController : Controller
+    {
+        private LiveNetworkDocumentationEntities _db;
+
+        public PersonnelController()
+        {
+            _db = new LiveNetworkDocumentationEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+
+        // GET: Personnel
+        public ActionResult Index()
+        {
+            var personelList = _db.KhadamatMashiniPersonnels.Include(k => k.Semat).Include(k => k.Manategh).ToList();
+
+            return View(personelList);
+        }
+
+        public ActionResult Edit(KhadamatMashiniPersonnel personnel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("PersonnelForm");
+            }
+
+            if (personnel.Id == 0)
+            {
+                _db.KhadamatMashiniPersonnels.Add(personnel);
+            }
+            else
+            {
+                var personnelInDb = _db.KhadamatMashiniPersonnels.Single(k => k.Id == personnel.Id);
+                personnelInDb.Name = personnel.Name;
+                personnelInDb.SematId = personnel.SematId;
+                personnelInDb.TelDakheli = personnel.TelDakheli;
+                personnelInDb.TelMostaghim = personnel.TelMostaghim;
+                personnelInDb.Mobile = personnel.Mobile;
+                personnelInDb.ManateghId = personnel.ManateghId;
+
+            }
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult New()
+        {
+            var personnelViewModel = new PersonnelSematMantagheViewModel();
+
+
+            return View("PersonnelForm", personnelViewModel);
+        }
+    }
+}
---
+++ LiveNetworkDocumentation/LiveNetworkDocumentation/ViewModels/PersonnelSematMantagheViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using LiveNetworkDocumentation.Models;
+
+namespace LiveNetworkDocumentation.ViewModels
+{
+    public class PersonnelSematMantagheViewModel
+    {
+        public byte Id { get; set; }
+
+        [Display(Name = "نام مسئول خدمات ماشینی")]
+        public string Name { get; set; }
+
+        public Semat SematId { get; set; }
+
+        [Display(Name = "سِمَت")]
+        public IEnumerable<Semat> Semats { get; set; }
+
+        [Display(Name = "تلفن داخلی")]
+        public string TelDakheli { get; set; }
+
+        [Display(Name = "تلفن مستقیم")]
+        public string TelMostaghim { get; set; }
+
+        [Display(Name = "موبایل")]
+        public string Mobile { get; set; }
+
+        public Manategh MantagheId { get; set; }
+
+        [Display(Name = "منطقه")]
+        public IEnumerable<Manategh> Manateghs { get; set; }
+
+    }
+}
